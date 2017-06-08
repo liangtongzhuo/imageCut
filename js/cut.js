dbb47f701bfce424948397f51d853992dbda482f
hi363138911 <liangya363138911@126.com>
2017-06-08 19:14:53 +0800

    update

--- js/cut.js
+++ js/cut.js
@@ -6,36 +6,45 @@ window.onload = function() {
     //设置预览图大小
     var widthSmall = 200;
     var heightSmall = 200;
-
+    //图片路径
+    var imgSrc = 'img/logo.jpg';
 
     //获取图片,并且记录图片大小比例
     var img1, img2, widthSacle, heightScale;
     var input = document.getElementById("imageFile");
+    img1 = document.getElementById("img1");
+    img2 = document.getElementById("img2");
+    img1.src = imgSrc;
+    img2.src = imgSrc;
+    //图片加载成功
+    img1.onload = function() {
+        var img = new Image();
+        img.src = img1.src;
+
+        widthSacle = widthMax / img.width;
+        heightScale = heightMax / img.height;
+
+        img1.width = widthMax;
+        img1.height = heightMax;
+
+        img2.width = widthMax;
+        img2.height = heightMax;
+
+        setPreview(); //预览一下
+    }
+    //点击选择图片后触发
     input.addEventListener('change', function() {
         var file = this.files[0];
-        img1 = document.getElementById("img1");
-        img2 = document.getElementById("img2");
-
         img1.src = window.URL.createObjectURL(file);
         img2.src = window.URL.createObjectURL(file);
-
-        //图片加载成功
-        img1.onload = function() {
-            var img = new Image();
-            img.src = img1.src;
-
-            widthSacle = widthMax / img.width;
-            heightScale = heightMax / img.height;
-
-            img1.width = widthMax;
-            img1.height = heightMax;
-
-            img2.width = widthMax;
-            img2.height = heightMax;
-
-            setPreview(); //预览一下
-        }
     });
+
+    //点击了
+    var btn = document.getElementById("button");
+    btn.onclick = function() {
+
+    }
+
 
     document.onselectstart = new Function('event.returnValue=false;'); //防止选中蓝色提示， 不明白注释了以后，尝试拖拽看看效果。
     $("#main").draggable({
@@ -141,9 +150,9 @@ window.onload = function() {
     //右边移动
     function rightMove(e) {
         var x = e.clientX; //鼠标X坐标
-        var addWidth = ""; //鼠标移动后选取框增加的宽度
         var widthBefore = mainDiv.offsetWidth - 2; //选取框变化前的宽度
-        addWidth = x - getPosition(mainDiv).left - widthBefore; //鼠标移动后增加的宽度
+        var addWidth = x - getPosition(mainDiv).left - widthBefore; //鼠标移动后增加的宽度;
+        if (addWidth + widthBefore > widthMax) return;
         mainDiv.style.width = addWidth + widthBefore + "px"; //选取框变化后
     }
     //上边移动
@@ -152,6 +161,7 @@ window.onload = function() {
         var mainY = getPosition(mainDiv).top; //选取框相对于屏幕上边的距离
         var addHeight = mainY - y; //增加的高度
         var heightBefore = mainDiv.offsetHeight - 2; //原来的高度
+        if (heightBefore + addHeight > heightMax) return;
         mainDiv.style.height = heightBefore + addHeight + "px";
         mainDiv.style.top = mainDiv.offsetTop - addHeight + "px";
     }
@@ -161,6 +171,7 @@ window.onload = function() {
         var mainX = getPosition(mainDiv).left; //选取框相对于屏幕左边的距离
         var addWidth = mainX - x; //增加的宽度
         var widthBefore = mainDiv.offsetWidth - 2; //原来的宽度
+        if (widthBefore + addWidth > widthMax) return;
         mainDiv.style.width = widthBefore + addWidth + "px";
         mainDiv.style.left = mainDiv.offsetLeft - addWidth + "px";
     }
@@ -170,6 +181,7 @@ window.onload = function() {
         var heightBefore = mainDiv.offsetHeight - 2; //原来的高度
         var mainY = getPosition(mainDiv).top; //选取框相对于屏幕上边的距离
         var addHeight = y - heightBefore - mainY; //增加的高度
+        if (addHeight + heightBefore > heightMax) return;
         mainDiv.style.height = addHeight + heightBefore + "px";
     }
 
